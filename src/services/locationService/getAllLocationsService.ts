b0b52e450a447eed2f5e99688dacf2bc25431c37
Palhanoo <bpalhano00@gmail.com>
2022-03-09 21:07:02 -0300

    alterando resposta

--- src/services/locationService/getAllLocationsService.ts
+++ src/services/locationService/getAllLocationsService.ts
@@ -1,13 +1,21 @@
 import { getRepository } from "typeorm";
 import { Location } from "../../database/entities/Location";
 
-
 export class GetAllLocationsService {
     async getAll() {
         const repo = getRepository(Location);
 
         const allLocations = await repo.find();
 
-        return allLocations;
+        let data = [];
+
+        for( const location of allLocations) {
+            data.push({
+                latitude: Number(location.latitude),
+                longitude: Number(location.longitude)
+            })
+        }        
+
+        return data;
     }
 }
