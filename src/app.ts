a920d9ec3a8daa0feeeb94944480041200d81934
Palhanoo <bpalhano00@gmail.com>
2022-03-11 09:34:53 -0300

    port

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ import { router } from './routes';
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 
 app.use(router)
 
-app.listen(3000, '0.0.0.0', () => console.log('server running'))
+app.listen(PORT, () => console.log('server running'))
