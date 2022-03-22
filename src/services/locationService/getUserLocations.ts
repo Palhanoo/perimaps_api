ee9f1f11b22d13c7be3b01c4225a41d497d2629a
Palhanoo <bpalhano00@gmail.com>
2022-03-22 18:18:25 -0300

    returning true

--- src/services/locationService/getUserLocations.ts
+++ src/services/locationService/getUserLocations.ts
@@ -4,18 +4,22 @@ import { Location } from "../../database/entities/Location";
 export class GetAllUserLocationsService {
     async getAll(user_id: string) {
         const repo = getRepository(Location);
+        try{
 
-        const allUserLocations = await repo.find({where: {user_id: user_id}});
-
-        let data = [];
-
-        for(const location of allUserLocations) {
-            data.push({
-                latitude: Number(location.latitude),
-                longitude: Number(location.longitude)
-            })
-        }        
-
-        return data;
+            const allUserLocations = await repo.find({where: {user_id: user_id}});
+    
+            let data = [];
+    
+            for(const location of allUserLocations) {
+                data.push({
+                    latitude: Number(location.latitude),
+                    longitude: Number(location.longitude)
+                })
+            }        
+    
+            return {success: true, locations: data};
+        }catch(error) {
+            return {success: false, message: 'Algo deu errado'}
+        }
     }
 }
