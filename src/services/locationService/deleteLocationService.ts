8cf2fb1a8a8dcd2c558b91f6b04740440534c22c
Palhanoo <bpalhano00@gmail.com>
2022-04-07 17:43:32 -0300

    adding id

--- src/services/locationService/deleteLocationService.ts
+++ src/services/locationService/deleteLocationService.ts
@@ -11,10 +11,10 @@ export class DeleteLocationService {
         const repo = getRepository(Location);
 
         const specificLocation = await repo.findOne({where: {latitude: location.latitude, longitude: location.longitude}});
-
+        
         if(!specificLocation) throw new Error('Localidade não encontrada')
 
-        const deletedLocation = await repo.delete(specificLocation);
+        const deletedLocation = await repo.delete(specificLocation.id);
 
         return deletedLocation;
     }
